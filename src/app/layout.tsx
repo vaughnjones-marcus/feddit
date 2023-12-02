ed6e1c5954282ab2c6c44bdfe375fbad64e598ba
vaughnjones-marcus <vaughnjones.marcus@gmail.com>
2023-12-02 12:32:24 -0600

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import '@/styles/globals.css'
+import {Inter} from "next/font/google"
+
+export const metadata = {
+  title: 'Feddit',
+  description: 'A Reddit clone built with Next.js and TypeScript.',
+}
+const inter = Inter ({ subsets: ['latin']})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  )
+}
